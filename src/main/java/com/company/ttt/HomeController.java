5d94548d4a898a3126c64c89753580b931bef015
Hyunsik Philip Choi <nanominos@gmail.com>
2020-02-16 19:39:41 +0900

    add SearchResultDTO, SearchItemDTO files.

--- src/main/java/com/company/ttt/HomeController.java
+++ src/main/java/com/company/ttt/HomeController.java
@@ -1,6 +1,13 @@
 package com.company.ttt;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Locale;
 
 import javax.inject.Inject;
 
@@ -9,12 +16,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.company.ttt.service.HomeService;
 import com.copany.dto.SearchDTO;
-
+import com.company.dto.SearchItemDTO;
+import com.company.dto.SearchResultDTO;
+import javax.servlet.http.*;
+//import com.company.ttt.service.HomeService;
 /**
  * Handles requests for the application home page.
  */
@@ -22,11 +33,56 @@ import com.copany.dto.SearchDTO;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	@Inject
-	private HomeService homeService;
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	
+	private static void getMemberFields(Object obj) throws IllegalAccessException,
+    NoSuchFieldException{
+		 HashMap<String, Object> fieldValues = new HashMap<String, Object>();
+		        Class<?> objClass = obj.getClass();
+		
+		Field[] fields = objClass.getDeclaredFields();
+		for(Field field : fields)
+		{
+		    field.setAccessible(true);
+		    fieldValues.put(field.getName(), field.get(obj));
+		    if (!field.getType().isPrimitive() && !field.getType().getName().contains("java.lang"))
+		    {
+		        getMemberFields(field.get(obj));
+		    }
+		}
+		Iterator<String> keys = fieldValues.keySet().iterator();
+		while( keys.hasNext() ){
+            String key = keys.next();
+            if (key == "list") {
+            	for (SearchItemDTO item: (ArrayList<SearchItemDTO>)fieldValues.get(key)) {
+            		getMemberFields(item);
+            	}
+            }
+            logger.info( String.format("키 : %s, 값 : %s", key, fieldValues.get(key)) );
+        }
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home_philip(Model model) throws IOException, IllegalAccessException, NoSuchFieldException {
+		/*
+		ApiRequester tester = new ApiRequester();
+		String val = "";
+		try  {
+			val = tester.Requester(ApiRequester.test_api_search_uri);
+		} catch (IOException e) {
+			logger.debug("IOException error");
+		}
+		
+		model.addAttribute("testvalue", val );
+		
+		//return "home";
+		 */
+		JsonParser parser = new JsonParser();
+		SearchResultDTO result = parser.test(JsonParser.test_val);
+		getMemberFields(result);
+		
+		return "test";
+	}
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(@ModelAttribute("searchDTO")SearchDTO searchDTO,Model model) {
 		
@@ -43,6 +99,8 @@ public class HomeController {
 		return "home";
 		//return "test";
 	}
+	
+	
 //	@RequestMapping(value="getList",method = RequestMethod.POST) 
 //	public String getList(@ModelAttribute("searchDTO")SearchDTO searchDTO) throws Exception {
 //		logger.info("getList");
